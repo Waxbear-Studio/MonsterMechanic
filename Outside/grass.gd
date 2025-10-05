84b700baa0718fc36c72fb598c90937c108fa05b
Waxbear-Studio <236215355+Waxbear-Studio@users.noreply.github.com>
2025-10-05 17:23:36 +0200

    First version and Set-Up

---
+++ Outside/grass.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	queue_free()
